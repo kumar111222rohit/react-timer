f15cd079cce056d6a67904819cf92cd0bdf44cc8
kumar_rohit <kumar111222rohit@gmail.com>
2024-02-27 16:02:45 +0530

    chore: add code-splitting at route level

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,12 +1,16 @@
-import { Routes, Route } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
+import { Suspense } from 'react';
+
 import CurrentTime from '../components/ReactTime1';
 import MouseMoveTimer from '../components/ReactTime2';
 
 const AppRoutes = () => (
-  <Routes>
-    <Route path="/react-timer" element={<CurrentTime />} />
-    <Route path="/mouse-timer" element={<MouseMoveTimer />} />
-  </Routes>
+  <Suspense fallback={<div>Loading...</div>}>
+    <Routes>
+      <Route path="/react-timer" element={<CurrentTime />} />
+      <Route path="/mouse-timer" element={<MouseMoveTimer />} />
+    </Routes>
+  </Suspense>
 );
 
 export default AppRoutes;
